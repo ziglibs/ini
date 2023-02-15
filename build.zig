23fd4a9748a191cd93c82f3d6f10c700a8f77dbf
Nguyễn Gia Phong <mcsinyx@disroot.org>
2023-02-15 17:12:07 +0900

    Update against Zig's new build API
    References: https://github.com/ziglang/zig/pull/14498

--- build.zig
+++ build.zig
@@ -1,36 +1,51 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("ini", "src/lib.zig");
+    const lib = b.addStaticLibrary(.{
+        .name = "ini",
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = b.standardTargetOptions(.{}),
+        .optimize = optimize,
+    });
     lib.bundle_compiler_rt = true;
     lib.addIncludePath("src");
     lib.linkLibC();
-    lib.setBuildMode(mode);
     lib.install();
 
-    const example_c = b.addExecutable("example-c", null);
+    const example_c = b.addExecutable(.{
+        .name = "example-c",
+        .optimize = optimize,
+    });
     example_c.addCSourceFile("example/example.c", &[_][]const u8{ "-Wall", "-Wextra", "-pedantic" });
     example_c.addIncludePath("src");
     example_c.linkLibrary(lib);
     example_c.linkLibC();
-    example_c.setBuildMode(mode);
     example_c.install();
 
-    const example_zig = b.addExecutable("example-zig", "example/example.zig");
-    example_zig.addPackagePath("ini", "src/ini.zig");
-    example_zig.setBuildMode(mode);
+    const example_zig = b.addExecutable(.{
+        .name = "example-zig",
+        .root_source_file = .{ .path = "example/example.zig" },
+        .optimize = optimize,
+    });
+    example_zig.addAnonymousModule("ini", .{
+        .source_file = .{ .path = "src/ini.zig" },
+    });
     example_zig.install();
 
-    var main_tests = b.addTest("src/test.zig");
-    main_tests.setBuildMode(mode);
+    var main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/test.zig" },
+        .optimize = optimize,
+    });
 
-    var binding_tests = b.addTest("src/lib-test.zig");
+    var binding_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib-test.zig" },
+        .optimize = optimize,
+    });
     binding_tests.addIncludePath("src");
     binding_tests.linkLibrary(lib);
     binding_tests.linkLibC();
-    binding_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
