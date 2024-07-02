da0af3a32e3403e3113e103767065cbe9584f505
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-07-02 11:36:35 +0100

    build: port to zig version 0.13.0

--- build.zig
+++ build.zig
@@ -5,20 +5,18 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     _ = b.addModule("ini", .{
-        .root_source_file = .{
-            .path = "src/ini.zig",
-        },
+        .root_source_file = b.path("src/ini.zig"),
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "ini",
-        .root_source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     lib.bundle_compiler_rt = true;
-    lib.addIncludePath(.{ .path = "src" });
+    lib.addIncludePath(b.path("src"));
     lib.linkLibC();
 
     b.installArtifact(lib);
@@ -29,16 +27,14 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
     example_c.addCSourceFile(.{
-        .file = .{
-            .path = "example/example.c",
-        },
+        .file = b.path("example/example.c"),
         .flags = &.{
             "-Wall",
             "-Wextra",
             "-pedantic",
         },
     });
-    example_c.addIncludePath(.{ .path = "src" });
+    example_c.addIncludePath(b.path("src"));
     example_c.linkLibrary(lib);
     example_c.linkLibC();
 
@@ -46,7 +42,7 @@ pub fn build(b: *std.Build) void {
 
     const example_zig = b.addExecutable(.{
         .name = "example-zig",
-        .root_source_file = .{ .path = "example/example.zig" },
+        .root_source_file = b.path("example/example.zig"),
         .optimize = optimize,
         .target = target,
     });
@@ -55,15 +51,15 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(example_zig);
 
     var main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/test.zig" },
+        .root_source_file = b.path("src/test.zig"),
         .optimize = optimize,
     });
 
     var binding_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/lib-test.zig" },
+        .root_source_file = b.path("src/lib-test.zig"),
         .optimize = optimize,
     });
-    binding_tests.addIncludePath(.{ .path = "src" });
+    binding_tests.addIncludePath(b.path("src"));
     binding_tests.linkLibrary(lib);
     binding_tests.linkLibC();
 
